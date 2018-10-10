d494e3c387a2135f0f9b51d634b1fe4c7599fa07
Fran Lucena <fran.lucena@gmail.com>
2018-10-10 19:32:25 +0200

    [UP] Enunciado del ejercicio MovimientoBancario

--- src/ficheros/declaracion.java
+++ src/ficheros/declaracion.java
@@ -5,6 +5,7 @@ package ficheros;
 
 // Clases para manejar la entrada/salida
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * @author franlu
@@ -17,12 +18,24 @@ public class declaracion {
 	 */
 	public static void main(String[] args) {
 		
-		File f1 = new File("\\datos\\info.txt");
+		 System.out.println ("Empezamos el programa");
+
+	        System.out.println ("Por favor introduzca una cadena por teclado:");
+
+	        String entradaTeclado = "";
+
+	        Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+
+	        entradaTeclado = entradaEscaner.nextLine (); //Invocamos un método sobre un objeto Scanner
+
+	        System.out.println ("Entrada recibida por teclado es: \"" + entradaTeclado +"\"");
+		
+		/**File f1 = new File("\\datos\\info.txt");
 
 		System.out.println(f1.getParent());
 		
 		String dir = "\\datos";
-		File directorio = new File(dir, "info1.txt");
+		File directorio = new File(dir, "info1.txt");*/
 	}
 
 }
