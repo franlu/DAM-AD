46835e87026b17410acdd62e0beaca6ced3300b3
franlu <fran.lucena@gmail.com>
2018-11-21 18:47:51 +0100

    [UP] Nombre del driver

--- UD2-Conectores/mysql/Procedimiento.java
+++ UD2-Conectores/mysql/Procedimiento.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Procedimiento {
 	public static void main(String[] args) {
 		try {
-			Class.forName("com.mysql.jdbc.Driver"); // Cargar el driver
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el driver
 			Connection conexion = DriverManager.getConnection(
 					"jdbc:mysql://localhost/ejemplo", "ejemplo",
 					    "ejemplo");
